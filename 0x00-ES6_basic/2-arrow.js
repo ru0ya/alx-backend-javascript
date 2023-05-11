544e39d7dc93c055315e774920ba6a7999ec60a0
ru0ya <viktaangi02@gmail.com>
2023-05-11 09:31:36 +0300

    debug

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -2,7 +2,8 @@ export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
 
-  this.addNeighborhood = (newNeighborhood) => {
+	const self = this;
+	this.addNeighborhood = (newNeighborhood) => {
     this.sanFranciscoNeighborhoods.push(newNeighborhood);
     return this.sanFranciscoNeighborhoods;
   };
