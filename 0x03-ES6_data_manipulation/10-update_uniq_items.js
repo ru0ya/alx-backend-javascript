b0458e827657956021d1563fc51b10c4720b382e
ru0ya <viktaangi02@gmail.com>
2023-05-12 23:36:07 +0300

    debug

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,17 +1,14 @@
-export default function updateUniqueItems(groceriesMap) {
-  if (!(groceriesMap instanceof Map)) {
+/* eslint-disable */
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
     throw new Error('Cannot process');
   }
 
-  const updatedMap = new Map();
-
-  groceriesMap.forEach((quantity, item) => {
-    if (quantity === 1) {
-      updatedMap.set(item, 100);
-    } else {
-      updatedMap.set(item, quantity);
+  map.forEach((value, key) => {
+    if (value === 1) {
+      map.set(key, 100);
     }
   });
 
-  return updatedMap;
+  return map;
 }
