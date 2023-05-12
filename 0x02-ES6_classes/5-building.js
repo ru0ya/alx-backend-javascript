f1fdd3bf20d3c9819b16a4c0da022dd8adbab63a
ru0ya <viktaangi02@gmail.com>
2023-05-13 00:27:57 +0300

    debug

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,14 +1,17 @@
-/* eslint-disable */
 export default class Building {
   constructor(sqft) {
-    this._sqft = typeof sqft === 'number' ? sqft : 0;
+    if (!(this instanceof Building)) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+
+    if (typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+
+    this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
